54c757e44ebc94e3da5454404be2933740dac46a
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2017-01-03 11:34:14 +0100

    Refactoring

--- index.js
+++ index.js
@@ -2,43 +2,64 @@
 var restify = require('restify');
 var discord = require('discord.js');
 
-// Create server
-var server = restify.createServer({
-    name: 'Discord Jenkins Notify'
-});
-
-// Configure restify
-restify.CORS.ALLOW_HEADERS.push('authorization');
-server.pre(restify.CORS());
-server.use(restify.authorizationParser());
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
-
 // Discord bot
 var bot    = new discord.Client({autoReconnect:true});
 var token  = "";
 var secret = "";
 
-bot.login(token);
+// Create and configure server
+var server = restify.createServer({name: 'Discord Jenkins Notify'});
 
-server.post('/', function (req, res, next)
+server.pre(restify.CORS());
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+// Start server and Discord bot
+server.listen(8082, function()
 {
-    var job    = req.body.name;
-    var build  = req.body.build.number;
-    var secret = req.query.secret;
+    console.log('%s listening at %s', server.name, server.url);
+    
+    // Login
+    bot.login(token)
+    .then(console.log('Bot Logged in.'))
+    .catch(
+        error => console.log(error)
+    );
+});
 
-    if (secret != secret)
+// General endpoint for browsers
+server.get('/', function (req, res, next)
+{
+    res.send("This address is not meant to be accessed by a web browser.")
+})
+
+// Endpoint for the Jenkins Notification plugin
+server.post('/jenkins', function (req, res, next)
+{
+    try
+    {
+        var job       = req.body.name;
+        var build     = req.body.build.number;
+        var reqsecret = req.query.secret;
+
+        // Set build status
+        if (req.body.build.status == "SUCCESS")
+        {
+            var status = "passed";
+        }
+        else
+        {
+            var status = "failed";
+        } 
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+    // Check secret
+    if (secret != reqsecret)
     {
         res.send(403, "Unauthorized");
-    }
-
-    if (req.body.build.status == "SUCCESS")
-    {
-        var status = "passed";
-    }
-    else
-    {
-        var status = "failed";
     }
 
     // Get all channels where
@@ -53,9 +74,4 @@ server.post('/', function (req, res, next)
     })
 
     res.send("message delivered");
-});
-
-// Start server and listen to port 8082
-server.listen(8082, function() {
-  console.log('%s listening at %s', server.name, server.url);
 });
